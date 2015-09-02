d0eca9918629471d52bec9ac84cc8636bf1b72e5
Red Hendery <red.hendery@gmail.com>
2015-09-02 15:33:30 +1200

    challenge 2 complete
    challenge 2 complete

--- 2-die-class/my_solution.rb
+++ 2-die-class/my_solution.rb
@@ -1,14 +1,25 @@
 # Die Class 2: Arbitrary Symbols
 
 class Die
+  attr_accessor :labels
+
   def initialize(labels)
+    unless labels.length > 0
+      raise ArgumentError.new("Please enter a string")
+    end
+    @labels = labels
   end
 
   def sides
+    @labels.length
   end
 
   def roll
+    @labels[rand(@labels.length)]
   end
 end
 
 # DRIVER TESTS GO BELOW THIS LINE
+
+die_roll = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
+puts die_roll
