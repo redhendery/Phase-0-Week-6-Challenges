b166c73b124ba76da0e43bdf398d55aa6e53b2bd
Red Hendery <red.hendery@gmail.com>
2015-09-04 13:42:47 +1200

    some progress on challenge 3
    some progress on challenge 3, not yet completed

--- 3-guessing-game/my_solution.rb
+++ 3-guessing-game/my_solution.rb
@@ -1,11 +1,45 @@
 # Build a simple guessing game
 
+# Create questions
+# Create answers
+# Check if answer right or wrong
+# Tell user if right or wrong with "True" or "False"
+# Store user answers
+# Count number of questions
+# Count number of answers
+# End game once number of correct number of answer
+# Tell users how many guesses it took
+
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
   end
-  
-  # Make sure you define the other required methods, too
+
+  def guess(guess)
+    @guess = guess
+    if guess > @answer
+      return :high
+    elsif guess == @answer
+      return :correct
+    else gues < @answer
+      return :low
+    end
+  end
+
+  def solved?
+    @guess == @answer
+  end
+
+  def GuessingGame#solved?(guess)
+    game = GuessingGame.new(10)
+
+    game.solved?
+
+
 end
 
+
+
+
 # DRIVER TESTS GO BELOW THIS LINE
+
