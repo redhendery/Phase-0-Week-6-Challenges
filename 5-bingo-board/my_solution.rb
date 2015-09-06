3ceb5706b96b71c34c2e3bc4b0f321f824af0b8f
Red Hendery <red.hendery@gmail.com>
2015-09-06 16:59:35 +1200

    chalenges 5, 6 & 7
    solutions to chalenges 5, 6 & 7, not all working

--- 5-bingo-board/my_solution.rb
+++ 5-bingo-board/my_solution.rb
@@ -3,16 +3,24 @@
 # Pseudocode
 
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
-  #fill in the outline here
+# def letter
+# => @letter = ["B", "I", "N", "G", "O"].shuffle.first
+# => @ball = rand(1..100)
 
 # Check the called column for the number called.
-  #fill in the outline here
+# => puts "The ball is #{@number} #{@ball}"
 
 # If the number is in the column, replace with an 'x'
-  #fill in the outline here
+# => @bingo_board.collect! do |i|
+# => if i.include?(@ball)
+# => i[i.index(@ball)] = 'X'; i
+
+# I think my code does this but I cannot get it to generate it with an 'X' to be sure
 
 # Display a column to the console
-  #fill in the outline here
+  # => newgame = BingoBoard.new
+# => puts newgame.letter
+# => newgame.verify
 
 # Display the board to the console (prettily)
   #fill in the outline here
@@ -28,6 +36,32 @@ class BingoBoard
     @bingo_board = Array.new(5) {Array(5.times.map{rand(1..100)})}
   end
 
+  def new_board
+    @bingo_board.each{|row| p row}
+  end
+
+def letter
+  @letter = ["B", "I", "N", "G", "O"].shuffle.first
+  @ball = rand(1..100)
+  puts "The ball is #{@letter} #{@ball}"
+end
+
+def verify
+  @ball
+  @bingo_board.each{|row| p row}
+  @bingo_board.collect! do |i|
+   if i.include?(@ball)
+     i[i.index(@ball)] = 'X'; i
+   else
+     i
+   end
+  end
+end
+
 end
 
 # DRIVER TESTS GO BELOW THIS LINE
+
+newgame = BingoBoard.new
+puts newgame.letter
+newgame.verify
--- 6-validate-credit-card/my_solution.rb
+++ 6-validate-credit-card/my_solution.rb
@@ -7,7 +7,38 @@
 
 class CreditCard
 
-end
+  def initialize number
+    @number = num
+  end
+
+    def #check_card
+        #check_card.length == 16
+      elsif #check_card.length != 16
+          raise ArgumentError.new("Invalid Card Number")
+        end
+      end
+
+  def valid?
+    digits = ''
+    @number.split('').reverse.each_with_indez do |d,i|
+      digits += d if i%2 == 0
+      digits += (d.to_i*2).to_s if i%2 ==i
+    end
+
+    digits.split('').inject(0){|sum,d| sum+d.to_i}%10 == 0
+  end
+
+    if __FILE__ == $0
+      card = CreditCard.new(ARGV.join.chomp)
+      if card.check_card.valid?
+        retrun true
+      else
+        return false
+      end
+    end
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+
+p valid_card = "Valid Card, please proceed"
+p invalid_card = "Invalid card, please check card number and try again"
